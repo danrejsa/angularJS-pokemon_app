0019d420d283cdf49f1b168032962af3fa1abc8a
Daniel <danrerjsa001@gmail.com>
2019-07-07 12:07:47 +0100

    modified package.json

--- src/app/logic/models/pokemonId.ts
+++ src/app/logic/models/pokemonId.ts
@@ -1,24 +1,43 @@
 export class PokemonId {
-    name: string;
-    id: number;
-    sprites: Sprites;
+  name: string;
+  id: number;
+  sprites: Sprites;
+  weight: number;
+  height: number;
+  stats: string;
+  types: string;
+  abilities: string;
+  moves: string;
+  base_experience: number;
 
-    constructor(
-        name: string,
-        id: number,
-        sprites: Sprites
-    ) {
-        this.name = name;
-        this.id = id;
-        this.sprites = sprites;
-    }
+  constructor(
+    name: string,
+    id: number,
+    sprites: Sprites,
+    weight: number,
+    height: number,
+    stats: string,
+    types: string,
+    abilities: string,
+    moves: string,
+    base_experience: number
+  ) {
+    this.name = name;
+    this.id = id;
+    this.sprites = sprites;
+    this.weight = weight;
+    this.height = height;
+    this.stats = stats;
+    this.types = types;
+    this.abilities = abilities;
+    this.moves = moves;
+    this.base_experience = base_experience;
+  }
 }
 
 export class Sprites {
-    front_default: string;
-    constructor(
-        front_default: string
-    ) {
-        this.front_default = front_default;
-    }
+  front_default: string;
+  constructor(front_default: string) {
+    this.front_default = front_default;
+  }
 }
